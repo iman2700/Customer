833470a4b151ffd129e1e9e5a495f6cbfb64824b
iamiri <i.amiri@derun.virtux.nl>
2023-11-18 20:26:31 +0330

    add Repository interface

---
+++ Application/Contracts/Persistence/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using Application.DTOs;
+using Domain.Entitiy;
+
+namespace Application.Persistence;
+
+public interface ICustomerRepository:IAsyncRepository<Customer>
+{
+    Task<bool> IsEmailUnique(string emailAddress);
+    Task<bool> IsCustomerUnique(CustomerDto customer);
+}
