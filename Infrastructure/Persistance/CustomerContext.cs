9e9bdec629a28a0d23472b1a98a4b5d74b1f19a4
iamiri <i.amiri@derun.virtux.nl>
2023-11-18 20:24:54 +0330

    create db context

---
+++ Infrastructure/Persistance/CustomerContext.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Common;
+using Domain.Entitiy;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistance;
+
+public class CustomerContext: DbContext
+{
+    public CustomerContext(DbContextOptions<CustomerContext> option) : base(option) { }
+
+    public DbSet<Customer> Customers { get; set; }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<EntityBase>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "iman";
+                    entry.Entity.LastModifiedBy = "iman";
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "iman";
+                    break;
+
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
