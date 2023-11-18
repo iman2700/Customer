0ad2a8435da8cbae4517662e6b8458e27a7c7929
iamiri <i.amiri@derun.virtux.nl>
2023-11-18 20:23:25 +0330

    create domain layer for project

---
+++ Domain/Entitiy/Customer.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Entitiy;
+
+public class Customer: EntityBase
+{
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; }
+    public string BankAccountNumber { get; set; }
+}
