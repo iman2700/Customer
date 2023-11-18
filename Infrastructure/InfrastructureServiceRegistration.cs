73c601114197c7d1d97a786c66b632501370a669
iamiri <i.amiri@derun.virtux.nl>
2023-11-18 20:34:25 +0330

    add migaration file

---
+++ Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Application.Persistence;
+using Infrastructure.Persistance;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+       
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddDbContext<CustomerContext>(option =>
+           option.UseSqlServer(configuration.GetConnectionString("CustomerConnectionString")));
+
+        return services;
+
+    }
+}
